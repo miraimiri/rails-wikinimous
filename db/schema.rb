867d2534080a6b7fb1b5f20fc67c9be34769bf2d
Miriam Derenbach <miriam.derenbach@outlook.com>
2021-01-09 15:58:13 +0900

    works, no css yet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_09_051813) do
+ActiveRecord::Schema.define(version: 2021_01_09_054107) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
