13a11a00c502b092e9b2d2b8c245fa690045b806
Miriam Derenbach <miriam.derenbach@outlook.com>
2021-01-09 14:31:40 +0900

    setup and routing done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Index > New > Create > Show > Edit > Update > Destroy
+  # verb 'path', to: 'controller#action'
+  # get '/articles', to: 'articles#index'
+
+  # get '/articles/new', to: 'articles#new', as: :new_article
+  # post '/articles', to: 'articles#create'
+
+  # get '/articles/:id', to: 'articles#show', as: :article
+
+  # get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  # patch 'articles/:id', to: 'articles#update'
+
+  # delete 'articles/:id', to: 'articles#destroy'
+  resources :articles
 end
